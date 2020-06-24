c8ecf78490a043e15870ca60ea6277b380c80458
Justina <j.klimaite.91@gmail.com>
2020-06-24 12:50:26 +0300

    lemon, orange

---
+++ lemon.php
@@ -0,0 +1,6 @@
+<?php
+
+$backColor = 'yellow';
+$link = '<a style="color: green;" href="orange.php">Link to orange</a>'.'<br>';
+
+echo '<div style="width: 100%; height: 100vh; background-color:'.$backColor.'">'.$link.'</div>';
---
+++ orange.php
@@ -0,0 +1,5 @@
+<?php
+
+$backColor = 'orange';
+
+echo '<div style="width: 100%; height: 100vh; background-color:'.$backColor.'"></div>';
